137e6cd5563b9453b59fa1dae1f8c9a4ae28159d
Brock <brock.altug99@gmail.com>
2023-02-15 14:21:16 -0800

    java practice and learning

---
+++ src/unit03/conditionals/day11/Car.java
@@ -0,0 +1,14 @@
+package unit03.conditionals.day11;
+
+public class Car {
+    private String color;
+
+    public Car(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "color='" + color + '\'' + '}';
+    }
+}
---
+++ src/unit03/conditionals/day11/StringMethodsReview.java
@@ -0,0 +1,9 @@
+package unit03.conditionals.day11;
+
+public class StringMethodsReview {
+    public static void main(String[] args) {
+//        .contains()
+       String str1 = "Java";
+        System.out.println(str1.contains("av"));
+    }
+}
