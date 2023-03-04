4b951067e10119bfc45c2155038ac92bbbb53f66
Brock <brock.altug99@gmail.com>
2023-03-03 19:54:37 -0800

    DeclareVariables - different variable types

--- src/unit01/primitive/types/day02/DeclareVariables.java
+++ src/unit01/primitive/types/day02/DeclareVariables.java
@@ -2,13 +2,18 @@ package unit01.primitive.types.day02;
 
 public class DeclareVariables {
     public static void main(String[] args) {
+        //different variable types
+        //whole number
         int myAge;
+        //decimal number
         double gpa;
+        //true or false
         boolean isOpen;
+        //single char - 'C'
         char letterGrade;
 
                 int myNumber;
-//        boolean private;
+//       boolean private;
 //        double average;
         final double PI;
 //        integer numberOfStudents;
