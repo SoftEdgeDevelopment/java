9b10856bf5c8084c960d012599460a27495d0162
Brock <brock.altug99@gmail.com>
2023-03-03 20:11:40 -0800

    PrimitiveVsReference - practice with primitive types

--- src/unit01/primitive/types/day02/PrimitiveVsReference.java
+++ src/unit01/primitive/types/day02/PrimitiveVsReference.java
@@ -1,18 +1,19 @@
 package unit01.primitive.types.day02;
 
 public class PrimitiveVsReference {
-
+//practice with primitive types
     public static void main(String[] args) {
         int year = 2022;
-
         String name = "John";
-
+        //will print 2022
         System.out.println(year);
-
+        //will print "John"
         System.out.println(name);
-
+        //will print the length of "John" - 4 chars
         System.out.println(name.length());
-
+        //will print the same year
+        System.out.println(year++);
+        //++ will be initialized after
         System.out.println(year);
 
     }
