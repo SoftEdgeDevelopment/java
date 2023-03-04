9817e72f51b8642b4d18f6680d1c0dde6c4555f0
Brock <brock.altug99@gmail.com>
2023-03-03 20:08:45 -0800

    OperandAndResults - practice with math - PEMDAS

--- src/unit01/primitive/types/day02/OperandAndResults.java
+++ src/unit01/primitive/types/day02/OperandAndResults.java
@@ -1,5 +1,5 @@
 package unit01.primitive.types.day02;
-
+//practice with math - PEMDAS
 public class OperandAndResults {
     public static void main(String[] args) {
         System.out.println(9/2);
