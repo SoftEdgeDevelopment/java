a7c0e276d386272f237bffd7953a7e64d4720a77
Brock <brock.altug99@gmail.com>
2023-03-07 14:20:51 -0800

    CompoundAssignment - practice compound variables += / *= / %=

--- src/unit01/primitive/types/day03/CompoundAssignment.java
+++ src/unit01/primitive/types/day03/CompoundAssignment.java
@@ -2,10 +2,16 @@ package unit01.primitive.types.day03;
 
 public class CompoundAssignment {
     public static void main(String[] args) {
+        //practice compound variable += / *= / %=
+        //value of x starts at 3
         int x = 3;
+        //3 + 7 = 10
         x += 7;
+        //10 * 3 = 30
         x *= 3;
+        // 30 / 2 = 15
         x %= 2;
+        //0 remaining
         System.out.println(x);
     }
 }
