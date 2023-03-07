13b73aa65ed95312f52175e0258956e1be74832e
Brock <brock.altug99@gmail.com>
2023-03-07 14:31:52 -0800

    PrefixVsPostfix - prefix vs postfix increment

--- src/unit01/primitive/types/day03/PrefixVsPostfix.java
+++ src/unit01/primitive/types/day03/PrefixVsPostfix.java
@@ -2,18 +2,22 @@ package unit01.primitive.types.day03;
 
 public class PrefixVsPostfix {
     public static void main(String[] args) {
-        int x;
-//        x = 5;
-//        x++;
-//        System.out.println(x);
-//        x = 5;
-//        ++x;
-//        System.out.println(x);
-//        x = 5;
-//        System.out.println(x++);
-//        System.out.println(x);
-
+        //prefix vs postfix increment
+        int x = 5;
+        x++;
+        //x = 6 because increment after x
+        System.out.println(x);
+        x = 5;
+        ++x;
+        //x = 6 because increment before x
+        System.out.println(x);
+        x = 5;
+        //x = 5 because increment after x but inside ()
+        System.out.println(x++);
+        //value of increment added now x = 6
+        System.out.println(x);
         x=5;
+        //value of x = 6 because increment before x
         System.out.println(++x);
     }
 }
