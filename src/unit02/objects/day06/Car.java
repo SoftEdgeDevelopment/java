683fee2018bb998cc064c450b39f4a17f3e99405
Brock <brock.altug99@gmail.com>
2023-03-22 16:28:15 -0700

    Car / CarMain - practice with constructor overloading

--- src/unit02/objects/day06/Car.java
+++ src/unit02/objects/day06/Car.java
@@ -11,6 +11,24 @@ public class Car {
     public Car(String theColor, double theMileage) {
         this.color = theColor;
         this.mileage = theMileage;
+        System.out.println("new object with 2 parameters created");
+    }
+    //practice with constructor overloading
+    //creating a new constructor with only 1 parameter and 1 assigned value
+    public Car(String theColor){
+        color = theColor;
+        mileage = 0;
+        System.out.println("new object with 1 parameter created");
+    }
+    //new constructor with no parameters and assigned values
+    public Car(){
+        color = "Orange";
+        mileage = 0;
+    }
+    //create new public void method to display messages about the car information
+    public void carInfo(){
+        System.out.println("The cars color is " + color);
+        System.out.println("The car drove " + mileage + " miles");
     }
 
 }
--- src/unit02/objects/day06/CarMain.java
+++ src/unit02/objects/day06/CarMain.java
@@ -6,6 +6,16 @@ public class CarMain
         //call to Car class and create new object car1 with new parameters
         Car car1 = new Car("Blue",30);
         Car car2 = new Car("White", 1000);
+        //create new object calling to the second constructor with 1 parameter
+        Car car3 = new Car("Yellow");
+        //create new object calling to the third constructor with 0 parameters
+        Car car4 = new Car();
+        //will print out the stored data location
         System.out.println(car1);
+        //call to the carInfo public void to display all cars information
+        car1.carInfo();
+        car2.carInfo();
+        car3.carInfo();
+        car4.carInfo();
     }
 }
