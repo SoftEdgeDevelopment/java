15019c84abf8463e743bbd71a677b0d25dba3f6e
Brock <brock.altug99@gmail.com>
2023-03-22 17:04:29 -0700

    Car / CarMain - practice overloading constructors to display additional information

--- src/unit02/objects/day07/Car.java
+++ src/unit02/objects/day07/Car.java
@@ -1,27 +1,47 @@
 package unit02.objects.day07;
 
+//create new class for Car
 public class Car {
+    //private String variable for the cars color
     private String color;
+    //private double variable for the number in cars mileage
     private double mileage;
 
+    //generate constructor to get new variables assigned
     public Car(String color, double mileage) {
+        //assign new variables from main
         this.color = color;
         this.mileage = mileage;
     }
 
+    //public void created to display drive message
     public void drive(){
+        System.out.println("first constructor");
+        System.out.println("you started at " + mileage + " miles");
+        //mileage will have 15 added to it
+        //mileage = mileage + 15;
         mileage += 15;
-//     note for you: mileage = mileage + 15;
-        System.out.println("You drove 15 miles");
-        System.out.print("The updated mileage is ");
+        System.out.print("after driving, the updated mileage is ");
+        //will display the updated mileage after drive
         System.out.println(mileage);
+        System.out.println("-----------------------");
     }
 
     public void drive(int drivenMiles){
+        System.out.println("second constructor");
+        //same as mileage += drivenMiles;
         mileage = mileage + drivenMiles;
-        System.out.print("The updated mileage is ");
+        System.out.println("You drove " + drivenMiles + " miles");
+        System.out.print("after driving the updated mileage is ");
         System.out.println(mileage);
+        System.out.println("-----------------------");
     }
-
-
+    //override to string - right click - generate - toString()
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", mileage=" + mileage +
+                '}';
+    }
 }
--- src/unit02/objects/day07/CarMain.java
+++ src/unit02/objects/day07/CarMain.java
@@ -1,10 +1,20 @@
 package unit02.objects.day07;
 
+//main
 public class CarMain {
     public static void main(String[] args) {
+        //new object created with 2 parameters
         Car car1 = new Car("Blue", 0);
+        Car car2 = new Car("Green",30);
+        //will display the value of the car info override to string
         System.out.println(car1);
+        //will call to the second constructor with 1 parameter
+        car1.drive(30);
+        //will call to the first constructor with no parameters
         car1.drive();
+        //will call to the second constructor with 1 parameter
         car1.drive(50);
+        //will call to the first constructor with no parameters for car2
+        car2.drive();
     }
 }
