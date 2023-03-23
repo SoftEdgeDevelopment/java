2fc2e8988486a95264f52f1f3fd981b9f3e4401f
Brock <brock.altug99@gmail.com>
2023-03-22 18:14:28 -0700

    Rectangle / RectangleMain - create constructor, declare method to return the parameters of integer area value after it is calculated

--- src/unit02/objects/day07/Rectangle.java
+++ src/unit02/objects/day07/Rectangle.java
@@ -1,29 +1,29 @@
 package unit02.objects.day07;
 
 public class Rectangle {
-//    Create a rectangle Class
-//    height width
-//    one constructor.
-//    Create printArea() method width*height
-//    1) declare variables
+    //Create a rectangle Class
+    //height width
+    //one constructor.
+    //Create printArea() method width*height
+    //1) declare variables
     private int width;
     private int height;
-//    2) Declare constructor
+    //2) Declare constructor
 
     public Rectangle(int width, int height) {
         this.width = width;
         this.height = height;
     }
-//    3) declare print area method
+    //3) declare print area method
     public void printArea(){
-        //will multiply parameters to get new int for area
+            //will multiply parameters to get new int for area
         int area = width*height;
-        //display the area
+            //display the area
         System.out.println("The area of one rectangle is " + area);
     }
-//    4) declare a getArea method which returns the integer area value
-//    after it calculated the area.
-    //new public int for returned value
+    //4) declare a getArea method which returns the integer area value
+    // after it calculated the area.
+    // new public int for returned value
     public int getArea(){
         //must both be int because it will return to original
        int area = width*height;
