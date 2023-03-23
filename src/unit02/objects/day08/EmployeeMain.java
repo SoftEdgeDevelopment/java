8d8db77d46102a9675822cc06caddb7c21a06dc2
Brock <brock.altug99@gmail.com>
2023-03-22 18:47:38 -0700

    Employee/EmployeeMain - create constructor and methods with parameters to return the value of a variable

--- src/unit02/objects/day08/EmployeeMain.java
+++ src/unit02/objects/day08/EmployeeMain.java
@@ -2,9 +2,11 @@ package unit02.objects.day08;
 
 public class EmployeeMain {
     public static void main(String[] args) {
-        Employee employee1 = new Employee("John Doe",13.5);
-
-        employee1.increase(15);
-        System.out.println(employee1.getSalary());
+        Employee employee1 = new Employee("James",18.75);
+        //call employee1 to salaryIncrease method to increase the salary
+        employee1.salaryIncrease(3.42);
+        //display message with getSalary method to return the new value of the salary
+        System.out.println("The new salary is " + employee1.getSalary());
     }
+
 }
