ab9d60bfee5014ebe52a287f281b3175d31f6f50
Brock <brock.altug99@gmail.com>
2023-03-22 18:18:51 -0700

    Flashlight / FlashlightMain - create constructor for boolean expression and create methods to initialize true or false

--- src/unit02/objects/day08/FlashLight.java
+++ src/unit02/objects/day08/FlashLight.java
@@ -1,19 +1,23 @@
 package unit02.objects.day08;
-
+//new class for flashlight
 public class FlashLight {
-//    instance variable
+//    new private instance variable boolen true or false
     private boolean isOn;
-//    constructor
+//    constructor with no parameters set to false
     public FlashLight() {
         this.isOn = false;
     }
 
+    //new method to turn flashlight off
     public void turnOff(){
+        //assign variable to false
         isOn = false;
         System.out.println("Flaslight is off");
     }
 
+    //new method to turn flashlight on
     public void turnOn(){
+        //assign variable to true
         isOn = true;
         System.out.println("Flaslight is on");
     }
