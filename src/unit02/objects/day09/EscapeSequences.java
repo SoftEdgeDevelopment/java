459144b5da6ad8944d9f62494575f6ccca9f86fa
BrockAltug <brock.altug99@gmail.com>
2023-03-24 16:18:23 -0700

    EscapeSequences - practice adding quotations to string, tabbing and new line with escape sequence

--- src/unit02/objects/day09/EscapeSequences.java
+++ src/unit02/objects/day09/EscapeSequences.java
@@ -2,8 +2,12 @@ package unit02.objects.day09;
 
 public class EscapeSequences {
     public static void main(String[] args) {
+        //escape characters will are quotations inside of a string
         System.out.println("City: \"Boston, MA\"");
+        //this is not written correctly
         System.out.println("City: \"\\\"Boston, MA");
+        // \t is for tab
+        // \n is for writing on a new line
         System.out.println("City:\tBoston\nState:\tMA" );
     }
 }
