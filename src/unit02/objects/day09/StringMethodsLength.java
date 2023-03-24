506d6f2985700486a318a2874707ae8126b64343
BrockAltug <brock.altug99@gmail.com>
2023-03-24 16:24:37 -0700

    StringMethodsLength - print out the amount of characters contained in a string (String.length())

--- src/unit02/objects/day09/StringMethodsLength.java
+++ src/unit02/objects/day09/StringMethodsLength.java
@@ -3,8 +3,9 @@ package unit02.objects.day09;
 public class StringMethodsLength {
     public static void main(String[] args) {
         String str1 = "Programming";
-//        int lengthOfStr = str1.length();
-//        System.out.println(lengthOfStr);
+     //another way to write as a new variable   int lengthOfStr = str1.length();
+     //dispaly the value of the new variable   System.out.println(lengthOfStr);
+        //this will print out the amount of characters used in the length of the string
         System.out.println(str1.length());
 
     }
