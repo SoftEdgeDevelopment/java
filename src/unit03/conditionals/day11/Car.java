dc6b6e011f9fa7971aa4e423cff53f6c958827c6
BrockAltug <brock.altug99@gmail.com>
2023-03-27 14:26:39 -0700

    Car / CarMain - generate constructor / Override toString practice

--- src/unit03/conditionals/day11/Car.java
+++ src/unit03/conditionals/day11/Car.java
@@ -1,12 +1,15 @@
 package unit03.conditionals.day11;
 
 public class Car {
+    //new private variable for color
     private String color;
 
+    //generate constructor with new parameter and assign to private variable
     public Car(String color) {
         this.color = color;
     }
 
+    //override to write data as String
     @Override
     public String toString() {
         return "Car{" + "color='" + color + '\'' + '}';
