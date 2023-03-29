68668422e54c342551361e4656c1bf07ad5a0234
BrockAltug <brock.altug99@gmail.com>
2023-03-29 14:02:25 -0700

    WrapperClasses - gets an object value and wraps it into a class

--- src/unit03/conditionals/day11/WrapperClasses.java
+++ src/unit03/conditionals/day11/WrapperClasses.java
@@ -1,10 +1,15 @@
 package unit03.conditionals.day11;
 
 public class WrapperClasses {
+    //gets an object value and wraps it into a class
     public static void main(String[] args) {
+
         int a = 7;
+        //this primitive gets it as an object and autoboxes
         Integer b = 7;
+        //the math will be handled with objects since it is wrapped and unboxed, normally wont work with objects
         System.out.println(a+b);
+        //will display max value of int
         System.out.println(Integer.MAX_VALUE);
     }
 }
