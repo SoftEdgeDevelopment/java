a00d7b77221ac57a7708c6ff6bcd85a93ccfd494
BrockAltug <brock.altug99@gmail.com>
2023-03-29 19:19:53 -0700

    RandomMethodPractice - different methods to get random numbers

--- src/unit03/conditionals/day11/RandomMethodPractice.java
+++ src/unit03/conditionals/day11/RandomMethodPractice.java
@@ -2,11 +2,15 @@ package unit03.conditionals.day11;
 
 public class RandomMethodPractice {
     public static void main(String[] args) {
-//        random numbers between 0 and 10
-        System.out.println((int) (Math.random() * 11));
-//       numbers between 1 and 6
+        //random number between 0 and 10 as decimal
+        System.out.println((Math.random() * 11));
+        //cast as (int) to display full number
+//        random number between 0 and 10
+        System.out.println((int)(Math.random() * 11));
+//       random number between 1 and 6
         System.out.println((int) (Math.random() * 6) + 1);
-//        Selims question between 4 and 8
+//        Selims question random number between 4 and 8
+        //4 is the starting index location, the random numbers will be between 5 total values starting from 4 (4,5,6,7,8) 8 is the highest possible number
         System.out.println((int) (Math.random() * 5) + 4);
     }
 }
