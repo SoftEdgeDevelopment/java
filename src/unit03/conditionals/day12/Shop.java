74c1b8093859527203d475cf0dbcfdf01a0c7f35
BrockAltug <brock.altug99@gmail.com>
2023-03-29 20:07:13 -0700

    Shop / ShopMain - get user input to display information from other classes using if statement

--- src/unit03/conditionals/day12/Shop.java
+++ src/unit03/conditionals/day12/Shop.java
@@ -1,17 +1,23 @@
 package unit03.conditionals.day12;
-
+//new class for Shop
 public class Shop {
+    //new private variable for price
     private int price;
-
+    //generate constructor for private variable
     public Shop() {
+        //set price value to 25
         this.price = 25;
     }
-
+    //new method for calculating price with 1 parameter for quantity
     public int calculatePrice(int quantity){
+        //price value is 25
         price = 25;
+        //get the quantity value from user input in the main
         if(quantity>=5){
+            //if more than 5, price is decreased by 1 for each
             price--;
         }
+        //return the value of the quantity*price to calculatePrice
         return quantity*price;
     }
 }
