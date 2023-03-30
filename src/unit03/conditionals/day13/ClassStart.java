c07fc4058e73a10b34e81f473b82848a93f3af02
BrockAltug <brock.altug99@gmail.com>
2023-03-30 00:08:56 -0700

    ClassStart - quick if else practice

--- src/unit03/conditionals/day13/ClassStart.java
+++ src/unit03/conditionals/day13/ClassStart.java
@@ -6,11 +6,17 @@ public class ClassStart {
         * If there are 50 or more students class starts
         * else you talk about world cup
         * */
-        int numberOfStudents = 49;
-        if(numberOfStudents >= 50) {
-            System.out.println("Class Starts");
-        } else {
-            System.out.println("Chit chat about the World Cup!");
+        //new int for number of students
+        int students = 75;
+        //if 50 or more students
+        if (students >= 50){
+            //class starts
+            System.out.println("Class has started");
+        }
+        //else less than 50
+        else{
+            //talk about sports
+            System.out.println("Lets talk about the world cup!");
         }
     }
 }
