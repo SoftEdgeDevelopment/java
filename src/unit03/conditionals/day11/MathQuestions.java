1a18044dc9003703915934d25ec48c4f0a059ee0
BrockAltug <brock.altug99@gmail.com>
2023-03-29 19:28:46 -0700

    MathQuestions - practice with math class methods

--- src/unit03/conditionals/day11/MathQuestions.java
+++ src/unit03/conditionals/day11/MathQuestions.java
@@ -1,11 +1,15 @@
 package unit03.conditionals.day11;
-
+//practice with math class methods
 public class MathQuestions {
     public static void main(String[] args) {
+        //new int variables
         int a = 3;
         int b = 2;
+        //new variable double for math with decimals
         double result;
+        //find the square root of a^3 / the absolute value of a-b
         result = Math.sqrt(Math.pow(a,3)/Math.abs(a-b));
+        //display the result
         System.out.println(result);
     }
 }
