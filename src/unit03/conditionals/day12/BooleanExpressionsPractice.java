c81479ed48db2a65ce7ab6bf7518d17939576fef
BrockAltug <brock.altug99@gmail.com>
2023-03-29 19:33:39 -0700

    BooleanExpressionsPractice - practice with boolean expression true or false

--- src/unit03/conditionals/day12/BooleanExpressionsPractice.java
+++ src/unit03/conditionals/day12/BooleanExpressionsPractice.java
@@ -1,11 +1,14 @@
 package unit03.conditionals.day12;
-
+//practice with boolean expression true or false
 public class BooleanExpressionsPractice {
     public static void main(String[] args) {
+        //new variable for age
         int age = 21;
+        //new boolean true or false if age is equal to or over 21
         boolean is21OrAbove;
-
+        //checks if age is equal to or over 21
         is21OrAbove = age >= 21;
+        //displays true or false based on the value of the int variable
         System.out.println(is21OrAbove);
     }
 }
