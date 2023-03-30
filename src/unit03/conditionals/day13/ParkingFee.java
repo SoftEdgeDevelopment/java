eb4da3c96721e2fdfb98cd57e2c48bdd49936fa7
BrockAltug <brock.altug99@gmail.com>
2023-03-30 00:00:02 -0700

    ParkingFee - write an if else statement for parking fee rates when exceeding a certain amount of time

--- src/unit03/conditionals/day13/ParkingFee.java
+++ src/unit03/conditionals/day13/ParkingFee.java
@@ -1,20 +1,30 @@
 package unit03.conditionals.day13;
-
+//write an if else statement for parking fee rates when exceeding a certain amount of time
 public class ParkingFee {
     public static void main(String[] args) {
 //        parking fee
 //        less than or equal to 2 hours parking: 30 dollars
-        double hours = 2;
-//          else
-        double total;
-        if(hours <= 2){
-            total = 30;
-        } else {
-            total = 12 * hours;
-        }
-        System.out.println("The parking fee is " + total);
 //        hourly rate: 12 dollars
 //        Note: Make it a function if there is time.
 //        print out the invoice showing the price
+        //new double for rate per hour $
+        double rate = 12;
+        //new double for amount of hours
+        double hours = 5;
+        //new double for the total price fee
+        double fee;
+        if(hours <= 2){
+            //if less than or equal to 2 hours, fee is 30
+            fee = 30;
+            //tell user how much they owe
+            System.out.println("you owe: $" + fee);
+        }
+        //else if the hours is more than 2
+        else{
+            //the fee is hours * rate
+            fee = hours * rate;
+            //tell user how much they owe
+            System.out.println("you owe: $" + fee);
+        }
     }
 }
