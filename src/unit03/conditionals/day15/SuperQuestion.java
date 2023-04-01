0a849f08a2beccfacb8da3bd7374717d3427adb9
BrockAltug <brock.altug99@gmail.com>
2023-04-01 16:36:46 -0700

    SuperQuestion - quick if, else if, else review for maths

--- src/unit03/conditionals/day15/SuperQuestion.java
+++ src/unit03/conditionals/day15/SuperQuestion.java
@@ -1,20 +1,28 @@
 package unit03.conditionals.day15;
-
+//quick if, else if, else review for maths
 public class SuperQuestion {
     public static void main(String[] args) {
+        //new variables for the value of x, y, and z
         int x = 5;
         int y = -2;
         int z = 4;
+        //if y * 2 is less than x
         if ((y * 2) < x)
         {
+            //multiply y by -1 and return the value to y
             y *=-1;
+            //subtract the value of y from z and return z
             z -= y;
         }
+        //if z does not equal x
         if (z != x)
         {
+            //add x by 1
             x++;
+            //make the value of y equal to x plus z
             y = x + z;
         }
+        //display the new values of x, y, and z
         System.out.println("x: " + x + " y: " + y + " z: " + z);
     }
 }
