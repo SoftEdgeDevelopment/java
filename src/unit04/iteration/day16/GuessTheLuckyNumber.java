ddacd7f05deb3d3967d123bd917f794575d8ce63
BrockAltug <brock.altug99@gmail.com>
2023-04-02 15:06:55 -0700

    GuessTheLuckyNumber - do while loop to guess a lucky number

--- src/unit04/iteration/day16/GuessTheLuckyNumber.java
+++ src/unit04/iteration/day16/GuessTheLuckyNumber.java
@@ -1,17 +1,24 @@
 package unit04.iteration.day16;
 
 import java.util.Scanner;
-
+//do while loop to guess a lucky number
 public class GuessTheLuckyNumber {
     public static void main(String[] args) {
+        //new variable for lucky number
         int luckyNumber = 15;
+        //new variable to store user input
         int inputNumber;
+        //new scanner to get user input
+        Scanner scan = new Scanner(System.in);
+        //create a do while loop
         do {
+            //prompt the user for input
             System.out.println("Whats the lucky number");
-            Scanner scan = new Scanner(System.in);
+            //get users input
             inputNumber = scan.nextInt();
+            //while user input does not equal lucky number, repeat the do loop
         } while (luckyNumber != inputNumber);
-
+        //display congrats if they got the number
         System.out.println("Congrats");
     }
 }
