09b7aeab6dd9a2a886ae89444d99d95980766c6d
BrockAltug <brock.altug99@gmail.com>
2023-04-02 15:17:54 -0700

    SumOfFirstFiveNumbers - while loop for a counter to display the total of amount of numbers

--- src/unit04/iteration/day16/SumOfFirstFiveNumbers.java
+++ src/unit04/iteration/day16/SumOfFirstFiveNumbers.java
@@ -1,16 +1,20 @@
 package unit04.iteration.day16;
-
+//while loop for a counter to display the total of amount of numbers
 public class SumOfFirstFiveNumbers {
     public static void main(String[] args) {
-
+        //create a total outside starting value from 0
         int total = 0;
-        int counter=1;
-        while(counter<=5){
-
-            total = total + counter;
+        //new variable for the counter starting from 1
+        int counter = 1;
+        while (counter <= 5){
+            //total = total + counter (0+1)(1+2)(3+3)(6+4)(10+5)
+            total += counter;
+            //counter goes up while counter is less than or equal to 5
             counter++;
+            //print the total
+            System.out.println(total);
         }
 
-        System.out.println("The total is " + total );
+
     }
 }
