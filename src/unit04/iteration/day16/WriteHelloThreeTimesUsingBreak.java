b928617a603c8ef774640bf705b2b5ba68a469b8
BrockAltug <brock.altug99@gmail.com>
2023-04-02 14:46:18 -0700

    While05Example - while loop display a message with a counter until it reaches a certain amount

--- src/unit04/iteration/day16/WriteHelloThreeTimesUsingBreak.java
+++ src/unit04/iteration/day16/WriteHelloThreeTimesUsingBreak.java
@@ -2,9 +2,11 @@ package unit04.iteration.day16;
 
 public class WriteHelloThreeTimesUsingBreak {
     public static void main(String[] args) {
+        //new variable that is being controlled in the while loop
         int controlNumber=1;
-
+        //while whatever is inside this loop is true
         while(true){
+            //if the controlled number is greaterthan or equal to 4
             if(controlNumber>=4){
                 break;
             }
