949ba0c2635ce6f6c28d00c049d5ddf99b2bc9a4
BrockAltug <brock.altug99@gmail.com>
2023-04-06 16:18:23 -0700

    CountdownWithForLoop - basic for loop to  show a countdown

--- src/unit04/iteration/day17/CountdownWithForLoop.java
+++ src/unit04/iteration/day17/CountdownWithForLoop.java
@@ -1,8 +1,10 @@
 package unit04.iteration.day17;
-
+//basic for loop to  show a countdown
 public class CountdownWithForLoop {
     public static void main(String[] args) {
+        //starting from 5, until 0, countdown
         for (int i = 5; i >= 0; i--) {
+            //display the countdown
             System.out.println("Countdown " + i);
         }
     }
