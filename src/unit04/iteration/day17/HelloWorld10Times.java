0d1a64d4acfb65a938ed8c5acd9f41f4685da5b6
BrockAltug <brock.altug99@gmail.com>
2023-04-06 16:19:55 -0700

    HelloWorld10Times - basic for loop to print something to console a certain number of times

--- src/unit04/iteration/day17/HelloWorld10Times.java
+++ src/unit04/iteration/day17/HelloWorld10Times.java
@@ -1,8 +1,10 @@
 package unit04.iteration.day17;
-
+//basic for loop to print something to console a certain number of times
 public class HelloWorld10Times {
     public static void main(String[] args) {
+        //starting from 0 until 9, add 1
         for (int i = 0; i < 10; i++) {
+            //will print out 10 times
             System.out.println("Hello World");
         }
     }
