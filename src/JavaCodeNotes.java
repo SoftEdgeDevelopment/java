9737a324a21643218070f9a0a0a4b83738a2aff5
BrockAltug <brock.altug99@gmail.com>
2023-04-11 23:48:29 -0700

    useful notes for java - check how many of the same char is contained in a string

--- src/JavaCodeNotes.java
+++ src/JavaCodeNotes.java
@@ -207,6 +207,24 @@ public class JavaCodeNotes {
 //        //display this at the end of the for loop
 //        System.out.println("Something");
 //    }
+//       --------------------- check how many of the same char is contained in a string ---------------------
+//     public static void main(String[] args) {
+//        //create a new string for a word
+//        String str1 = "Argentina";
+//        //new count starting from 0
+//        int count = 0;
+//        //starting from 0, until the string length add 1
+//        for (int i = 0; i < str1.length(); i++) {
+//            //checks letter by letter using substring to see how many a's are contained in the String
+//            if (str1.substring(i, i + 1).contains("a") || str1.substring(i, i + 1).contains("A")){
+//                //if any a's are contained, increase the count
+//                count++;
+//            }
+//        }
+//        //display the number of a's in the String
+//        System.out.println("There are " + count + " \"a\"s in the " + str1);
+//
+//    }
 //       --------------------- method to check if a number is prime ---------------------
 //       ---
 //       public static void main(String[] args) {
