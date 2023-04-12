d5661813ada340f9e6e11d792516bbad94c7c84e
BrockAltug <brock.altug99@gmail.com>
2023-04-11 23:50:07 -0700

    useful notes for java - String Transversal and writing a word backwards

--- src/JavaCodeNotes.java
+++ src/JavaCodeNotes.java
@@ -207,6 +207,25 @@ public class JavaCodeNotes {
 //        //display this at the end of the for loop
 //        System.out.println("Something");
 //    }
+//    --------------------- String Transversal and writing a word backwards ---------------------
+//     public static void main(String[] args) {
+//        String str1 = "World";
+//        //starting at 1, until the Strings length, add 1
+//        for (int i = 0; i < str1.length(); i++) {
+//            //display the substring starting at i until i+1 (0,0+1) to get each index letter 1 at a time
+//            System.out.println(str1.substring(i, i+1));
+//        }
+//        System.out.println("+++++Reversed Version++++++++++");
+//        //starting at String legnth - 1, until 0, subtract 1
+//        for (int i = str1.length() - 1; i >=0 ; i--) {
+//            //display the substrings and +1 to display 1 letter at a time
+//            System.out.println(str1.substring(i, i+1));
+//        }
+//        System.out.println("+++++charAt() Version++++++++++");
+//        //same way to write as the first method with charAt instead of substring
+//        for (int i = 0; i < str1.length(); i++) {
+//            System.out.println(str1.charAt(i));
+//        }
 //       --------------------- check how many of the same char is contained in a string ---------------------
 //     public static void main(String[] args) {
 //        //create a new string for a word
