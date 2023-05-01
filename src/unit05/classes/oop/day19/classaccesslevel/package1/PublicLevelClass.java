4eaa354fcc63d66f319d8be02480443b1f77b77c
BrockAltug <brock.altug99@gmail.com>
2023-04-30 18:19:04 -0700

    remove unit 5 to start again

--- src/unit05/classes/oop/day19/classaccesslevel/package1/PublicLevelClass.java
+++
@@ -1,5 +0,0 @@
-package unit05.classes.oop.day19.classaccesslevel.package1;
-
-public class PublicLevelClass {
-    private int x;
-}
--- src/unit05/classes/oop/day19/mainmethods/ClassB.java
+++
@@ -1,9 +0,0 @@
-package unit05.classes.oop.day19.mainmethods;
-
-public class ClassB {
-
-    public static void main(String[] args) {
-        ClassC classC = new ClassC();
-        ClassA classA = new ClassA();
-    }
-}
