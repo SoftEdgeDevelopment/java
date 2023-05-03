8d6e40eb724bd5fe0822a77bba51b1310fe73a47
BrockAltug <brock.altug99@gmail.com>
2023-05-02 17:58:51 -0700

    different class access levels example

--- src/unit05/classes/oop/day19/classaccesslevel/mainpackage/MainMethodClass.java
+++ src/unit05/classes/oop/day19/classaccesslevel/mainpackage/MainMethodClass.java
@@ -1,6 +1,6 @@
 package unit05.classes.oop.day19.classaccesslevel.mainpackage;
 
-import unit05.classes.oop.day19.classaccesslevel.package1.PublicLevelClass;
+import unit05.classes.oop.day19.classaccesslevel.package01.PublicLevelClass;
 //will not work because it is a default class
 //import unit05.classes.oop.day19.ClassAccessLevel.package2.DefaultLevelClass;
 
---
+++ src/unit05/classes/oop/day19/classaccesslevel/package02/DefaultLevelClass.java
@@ -0,0 +1,5 @@
+package unit05.classes.oop.day19.classaccesslevel.package02;
+
+class DefaultLevelClass {
+    private int x;
+}
