8bab1990b49b979424f20d0425e07034d82a5968
BrockAltug <brock.altug99@gmail.com>
2023-05-06 16:08:32 -0700

    Encapsulation - practice with encapsulation, private, public, protected, default

---
+++ src/unit05/classes/oop/day19/encapsulation/EncapsulationExample.java
@@ -0,0 +1,9 @@
+package unit05.classes.oop.day19.encapsulation;
+
+public class EncapsulationExample {
+    private int privateNum;      //private only available within class
+    public int publicNum;       //public available anywhere
+    protected int protectedNum;        //protected only available within package or sub package
+    int defaultNum;      //default only available within package or sub package
+
+}
