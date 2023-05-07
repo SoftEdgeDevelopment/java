f29eca578cbf317f85605f0c36e8ff7252750c51
BrockAltug <brock.altug99@gmail.com>
2023-05-06 19:18:09 -0700

    Car - making constructor with no parameters, and a second constructor with parameters, assigning variables, override to string, create objects and call the new objects to main to display the info

--- src/unit05/classes/oop/day20/constructors/Car.java
+++ src/unit05/classes/oop/day20/constructors/Car.java
@@ -15,6 +15,15 @@ public class Car {
         mileage = 137489;
         automaticTransmission = true;
     }
+
+    //right click - generate - constructor - select all
+    public Car(String color, int year, double mileage, boolean automaticTransmission) {
+        this.color = color;
+        this.year = year;
+        this.mileage = mileage;
+        this.automaticTransmission = automaticTransmission;
+    }
+
     //display car 1 and all the cars information
     //right click - generate - to string - select all
     //press shift x2 - search softwrap
@@ -29,7 +38,13 @@ public class Car {
     }
     //main method
     public static void main(String[] args) {
+        //create object car1 using first constructor
         Car car1 = new Car();
+        //display the info of car1 called from generating to string
         System.out.println(car1);
+        //create object car2 using second constructor
+        Car car2 = new Car("Yellow", 2010, 145000, true);
+        //display the info of car2 from parameters assigned
+        System.out.println(car2);
     }
 }
