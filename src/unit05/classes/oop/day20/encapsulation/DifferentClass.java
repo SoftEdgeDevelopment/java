dbed31c5471f545853bdf3c35c74480464f4021c
BrockAltug <brock.altug99@gmail.com>
2023-05-06 17:54:02 -0700

    Encapsulation - examples of what can and cannot be reached from different class / package

---
+++ src/unit05/classes/oop/day20/encapsulation/DifferentClass.java
@@ -0,0 +1,17 @@
+package unit05.classes.oop.day20.encapsulation;
+//examples of what can and cannot be reached from different class / package
+public class DifferentClass {
+    public static void main(String[] args) {
+        EncapsulationExample a = new EncapsulationExample();
+        //only 1 that cannot be reached because it is not in the same class
+        //System.out.println(a.privateInt);
+        //can only be reached within the same package or sub package
+        System.out.println(a.defaultInt);
+        //can be reached from anywhere
+        System.out.println(a.publicInt);
+        //can only be reached within the same package or sub package
+        System.out.println(a.protectedInt);
+        a.publicInt = 17;
+        System.out.println(a.publicInt);
+    }
+}
