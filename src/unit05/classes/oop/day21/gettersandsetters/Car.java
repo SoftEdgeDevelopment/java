741df49f737fe69f90a6046ca17b7b1441021e86
BrockAltug <brock.altug99@gmail.com>
2023-05-07 16:22:59 -0700

    Car - car class created with getters to call from the main

---
+++ src/unit05/classes/oop/day21/gettersandsetters/Car.java
@@ -0,0 +1,27 @@
+package unit05.classes.oop.day21.gettersandsetters;
+//Car class created with getters to call from the main
+public class Car {
+    //create private variables for color, year, mileage
+    private String color;
+    private int year;
+    private int mileage;
+
+
+    //generate constructor with parameters
+    public Car(String color, int year, int mileage) {
+        this.color = color;
+        this.year = year;
+        this.mileage = mileage;
+    }
+
+    //create getter methods to get color, yea, and mileage information
+    public String getColor(){
+        return color;
+    }
+    public int getYear(){
+        return year;
+    }
+    public int getMileage(){
+        return mileage;
+    }
+}
