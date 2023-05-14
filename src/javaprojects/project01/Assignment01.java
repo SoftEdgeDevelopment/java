db767de506bb96c32e2624ca972c0850cd7e8a0a
BrockAltug <brock.altug99@gmail.com>
2023-05-13 20:44:31 -0700

    Assignment01 - prints the letter 'Q' to console

--- src/javaprojects/project01/Assignment01.java
+++ src/javaprojects/project01/Assignment01.java
@@ -1,4 +1,8 @@
 package javaprojects.project01;
 
 public class Assignment01 {
+    //Rearrange the following code so that it forms the correct program and prints the letter 'Q'
+    public static void main(String[] args) {
+        System.out.println('Q');
+    }
 }
