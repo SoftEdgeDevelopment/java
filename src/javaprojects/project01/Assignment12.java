af5a9b000d188b0527005c4959a601e885587891
BrockAltug <brock.altug99@gmail.com>
2023-05-13 20:53:47 -0700

    Assignment12 - create a variable degreesCelcius and assign it a value then print to console

---
+++ src/javaprojects/project01/Assignment12.java
@@ -0,0 +1,13 @@
+package javaprojects.project01;
+
+public class Assignment12 {
+    //create a variable degreesCelcius and assign it a value then print to console
+    public static void main(String[] args) {
+        //declare integer
+        int degreesCelcius;
+        //assign value
+        degreesCelcius = 35;
+        //print variable to console
+        System.out.println(degreesCelcius);
+    }
+}
