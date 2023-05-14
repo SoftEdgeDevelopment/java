26891776c36f6ff8f09f6a9473fcd128f9dbd885
BrockAltug <brock.altug99@gmail.com>
2023-05-13 20:45:09 -0700

    Assignment02 - write a program that displays "I think being a programmer is cool"

---
+++ src/javaprojects/project01/Assignment02.java
@@ -0,0 +1,18 @@
+package javaprojects.project01;
+
+public class Assignment02 {
+    //Write a program that displays "I think being a programmer is cool"
+    //Requirements:
+    /*the program should output text
+     * the text should begin with "I think"
+     * the text should end with "is cool"
+     * the text should consist of 34 characters, including spaces and punctuation marks
+     * the text should consist of one line
+     * the displayed text must match the task conditions
+     */
+    public static void main(String[] args) {
+        System.out.print("I think ");
+        System.out.print("being a programmer ");
+        System.out.print("is cool");
+    }
+}
