e4d88408ae794db9413a7b9b94028bf282a8832a
BrockAltug <brock.altug99@gmail.com>
2023-05-14 00:16:43 -0700

    Assignment15 - practice finding and correcting the mistakes

---
+++ src/javaprojects/project03/Assignment15.java
@@ -0,0 +1,38 @@
+package javaprojects.project03;
+
+//practice finding and correcting the mistakes
+public class Assignment15 {
+//     Fill in the blanks:
+//     public class 1) ___________ {
+//         String name;
+//         double price;
+// public Item(String name, double price){
+//             2) ___________;
+//             3) ___________;
+//         }
+//        public static void main(String [] args){
+//             4) ___________ = new Item( 5) ___________ );
+//         }
+//     }
+
+
+//--------------------results should be this
+    //1. Item
+    //2. this.name = name;
+    //3. this.price = price;
+    //4. Item item
+    //5. "Product Name", 10.0
+//-----------------------------------------------------------------
+
+//    public class Item{                               // #1
+//        String name;
+//        double price;
+//    public Item(String name, double price) {
+//        this.name = name;                            // #2
+//        this.price = price;                          // #3
+//    }
+//    public static void main(String[] args) {
+//        Item item1 = new Item("john",12);            // #4 , #5
+//    }
+//}
+}
