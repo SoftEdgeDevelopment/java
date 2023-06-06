9501788780858e49c78ea7dd689ea4bef636f2f6
BrockAltug <brock.altug99@gmail.com>
2023-06-06 00:00:20 -0700

    HelloWorld - Hello world! first java class

--- src/unit01/primitive/types/learning01/HelloWorld.java
+++ src/unit01/primitive/types/learning01/HelloWorld.java
@@ -2,6 +2,7 @@ package unit01.primitive.types.learning01;
 
 public class HelloWorld {
 //Hello world! first java class
+
     public static void main(String[] args){
 
         System.out.println("Hello World!");
