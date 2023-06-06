2997a1acb7f1e7c8d7255d70ed7fe92bd2ba1825
BrockAltug <brock.altug99@gmail.com>
2023-06-05 23:35:51 -0700

    FindMax - find the min value in the array and print it, and another example as a method

--- src/unit06/array/day25/arrayalgorithms/FindMax.java
+++ src/unit06/array/day25/arrayalgorithms/FindMax.java
@@ -1,5 +1,5 @@
 package unit06.array.day25.arrayalgorithms;
-//find the min value in the array and print it
+//find the min value in the array and print it, and another example as a method
 public class FindMax {
     public static void main(String[] args) {
         int[] numbers = {6345, 6, 456, 457, 45, 74, 56, 345, 2, 345, 23, 45, 236, 4};
@@ -16,5 +16,18 @@ public class FindMax {
         }
         // print the maximum number
         System.out.println("The maximum number in the array is " + max);
+        //call to the method to return the maximum number
+        System.out.println(returnMax(numbers));
+    }
+    //create it as a method
+    public static int returnMax(int[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        //return the value of the max to the method
+        return max;
     }
 }
