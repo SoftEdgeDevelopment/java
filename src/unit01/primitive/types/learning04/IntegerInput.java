3ce4c44d1fd2c09f409bca37a6dcb4c92fe1166f
BrockAltug <brock.altug99@gmail.com>
2023-06-06 00:10:04 -0700

    IntegerInput - get user input and display message for integer

--- src/unit01/primitive/types/learning04/IntegerInput.java
+++ src/unit01/primitive/types/learning04/IntegerInput.java
@@ -3,6 +3,7 @@ package unit01.primitive.types.learning04;
 import java.util.Scanner;
 
 public class IntegerInput {
+
     public static void main(String[] args) {
         //get user input and display message
         //create new Scanner for user input
