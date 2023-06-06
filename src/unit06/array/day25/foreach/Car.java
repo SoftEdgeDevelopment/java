7fad9824670c58ecbe254bd764b24188925a257f
BrockAltug <brock.altug99@gmail.com>
2023-06-05 22:51:14 -0700

    Car - create a class with private variables, constructors, and getters and setters, create a new array to hold objects, and display object info if the variable is more than a certain amount

--- src/unit06/array/day25/foreach/Car.java
+++ src/unit06/array/day25/foreach/Car.java
@@ -1,5 +1,6 @@
 package unit06.array.day25.foreach;
-//create a class with private variables, constructors, and getters and setters, create a new array to hold objects
+
+//create a class with private variables, constructors, and getters and setters, create a new array to hold objects, and display object info if the variable is more than a certain amount
 public class Car {
     // private variable for storing the mileage of the car
     private int mileage;
@@ -27,12 +28,20 @@ public class Car {
 
     public static void main(String[] args) {
         // create a new Car objects and assign a value to the mileage
-        Car car1 = new Car(1235934);
+        Car car1 = new Car(123);
         Car car2 = new Car(45234675);
         Car car3 = new Car(7567456);
 
         // create a new array called cars to hold the Car objects
         Car[] cars = {car1, car2, car3};
+        //foreach car element in the Car object, cars array
+        for (Car car : cars) {
+            //display the car if mileage is more than 50000
+            //car.mileage works as well
+            if(car.getMileage() >= 50000){
+                System.out.println(car);
+            }
+        }
 
     }
 }
