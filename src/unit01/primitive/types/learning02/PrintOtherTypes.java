db6442abeb9fa34de1629baa186eb810d7cb66f1
BrockAltug <brock.altug99@gmail.com>
2023-06-06 00:04:02 -0700

    PrintOtherTypes - practice printing to console

--- src/unit01/primitive/types/learning02/PrintOtherTypes.java
+++ src/unit01/primitive/types/learning02/PrintOtherTypes.java
@@ -1,7 +1,6 @@
 package unit01.primitive.types.learning02;
 
 public class PrintOtherTypes {
-
     public static void main(String[] args) {
         //practice printing to console
         System.out.println(2022);
