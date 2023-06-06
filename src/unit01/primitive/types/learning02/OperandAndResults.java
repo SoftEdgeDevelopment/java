1ec0ad06568e129e01acd19d937c31e73239f93b
BrockAltug <brock.altug99@gmail.com>
2023-06-06 00:03:34 -0700

    OperandAndResults - practice with math - PEMDAS

--- src/unit01/primitive/types/learning02/OperandAndResults.java
+++ src/unit01/primitive/types/learning02/OperandAndResults.java
@@ -1,6 +1,7 @@
 package unit01.primitive.types.learning02;
 //practice with math - PEMDAS
 public class OperandAndResults {
+
     public static void main(String[] args) {
         System.out.println(9/2);
         System.out.println( 3*4 + (14-7)/5 - 3%2);
