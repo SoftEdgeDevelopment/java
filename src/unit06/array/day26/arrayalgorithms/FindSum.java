8e36913eb0254c4b9ac5c3e1668c9cbee2c0f66f
BrockAltug <brock.altug99@gmail.com>
2023-06-06 17:46:34 -0700

    FindSum - call to the findSum method for the numbers array to find the total sum of the elements in the array

--- src/unit06/array/day26/arrayalgorithms/FindSum.java
+++ src/unit06/array/day26/arrayalgorithms/FindSum.java
@@ -6,10 +6,9 @@ public class FindSum {
         int[] numbers = {34, 64, 27, 23, 75, 234, 66};
         //call to the findSum method for the numbers array
         System.out.println(findSum(numbers));
-        //cast to double and find the sum of the array by calling the findSum method and diving by the length of the array
-        System.out.println((double)findSum(numbers) / numbers.length);
+        //call to the findAverage method to find the average of all the elements in the numbers array
+        System.out.println(findAverage(numbers));
     }
-
     public static int findSum(int[] arr) {
         //the starting sum is 0
         int sum = 0;
@@ -20,6 +19,17 @@ public class FindSum {
         }
         //return the sum to the method
         return sum;
+    }
+    public static double findAverage(int [] arr){
+        //the starting sum is 0
+        int sum = 0;
+        //foreach element in the array
+        for (int num : arr) {
+            //add each number to the value of the sum
+            sum += num;
+        }
+        //return the sum to the method
+        return ((double)sum / arr.length);
     }
 
 }
