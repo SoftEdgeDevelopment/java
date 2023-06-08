e8b8814423a2bcc7c2bc79657b5b497ae0ba39dd
BrockAltug <brock.altug99@gmail.com>
2023-06-08 01:44:29 -0700

    AddEvenNumbers - while loop for a counter to find the sum of the even numbers until a certain amount

--- src/unit04/iteration/learning16/AddEvenNumbers.java
+++ src/unit04/iteration/learning16/AddEvenNumbers.java
@@ -1,4 +1,4 @@
-package unit04.iteration.day16;
+package unit04.iteration.learning16;
 //while loop for a counter to find the sum of the even numbers until a certain amount
 public class AddEvenNumbers {
     public static void main(String[] args) {
