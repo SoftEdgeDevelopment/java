1e35aa97658718942d9dcb3930c7ecd2953d0c83
BrockAltug <brock.altug99@gmail.com>
2023-06-07 20:36:10 -0700

    MonthValidation - if else example using and

--- src/unit03/conditionals/learning14/MonthValidation.java
+++ src/unit03/conditionals/learning14/MonthValidation.java
@@ -1,4 +1,4 @@
-package unit03.conditionals.day14;
+package unit03.conditionals.learning14;
 //if else example using and
 public class MonthValidation {
     public static void main(String[] args) {
