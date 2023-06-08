919a3f5aaf4d30fcc614c7764aca9f523c87ce48
BrockAltug <brock.altug99@gmail.com>
2023-06-08 01:23:22 -0700

    ShiftRight - method to shift all elements in the array to the right

--- src/unit06/array/day26/shiftarrayalgorithms/ShiftRight.java
+++ src/unit06/array/day26/shiftarrayalgorithms/ShiftRight.java
@@ -3,7 +3,7 @@ package unit06.array.day26.shiftarrayalgorithms;
 public class ShiftRight {
     public static void main(String[] args) {
         //create an array with values
-        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array1 = {1, 2, 3, 4, 5,6,7,8};
         //create a new array assigned to the length of the first array
         int[] array2 = new int[array1.length];
         //starting from index zero until the last element in the array of the first array
@@ -13,6 +13,9 @@ public class ShiftRight {
         }
         //assign the value of the first element in the second array to the last element in the first array
         array2[0] = array1[array1.length - 1];
+        System.out.print("Original Array : ");
+        printArr(array1);
+        System.out.println();
         //assign the value of array1 to array2
         array1 = array2;
         //call to the printArr method to display the shifted array
