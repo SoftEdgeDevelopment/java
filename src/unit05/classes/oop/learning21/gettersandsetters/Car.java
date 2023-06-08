a035a8795bea295f1815d721fc6310d57d8d6d88
BrockAltug <brock.altug99@gmail.com>
2023-06-08 02:07:54 -0700

    Car - car class created with getters to call from the main and setters to change the values of parameters of the object in the main

--- src/unit05/classes/oop/learning21/gettersandsetters/Car.java
+++ src/unit05/classes/oop/learning21/gettersandsetters/Car.java
@@ -1,4 +1,4 @@
-package unit05.classes.oop.day21.gettersandsetters;
+package unit05.classes.oop.learning21.gettersandsetters;
 //Car class created with getters to call from the main and setters to change the values of parameters of the object in the main
 public class Car {
     //create private variables for color, year, mileage
