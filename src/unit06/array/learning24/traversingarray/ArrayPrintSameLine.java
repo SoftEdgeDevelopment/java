596763c2d65de63453ed045fab0e88a7f6130da3
BrockAltug <brock.altug99@gmail.com>
2023-06-08 02:15:37 -0700

    ArrayPrintSameLine - print arrays on the same line with a space between elements

--- src/unit06/array/learning24/traversingarray/ArrayPrintSameLine.java
+++ src/unit06/array/learning24/traversingarray/ArrayPrintSameLine.java
@@ -1,4 +1,4 @@
-package unit06.array.day24.traversingarray;
+package unit06.array.learning24.traversingarray;
 
 //print arrays on the same line with a space between elements
 public class ArrayPrintSameLine {
