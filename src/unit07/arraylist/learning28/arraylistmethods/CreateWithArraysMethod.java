fc71c44c750f9b53578a3db6e6ef1e527f2c0756
BrockAltug <brock.altug99@gmail.com>
2023-07-21 01:30:45 -0700

    CreateWithArraysMethod - array list as method using collection to display list of numbers

--- src/unit07/arraylist/learning28/arraylistmethods/CreateWithArraysMethod.java
+++ src/unit07/arraylist/learning28/arraylistmethods/CreateWithArraysMethod.java
@@ -9,5 +9,15 @@ public class CreateWithArraysMethod {
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
         //display the numbers from the array list
         System.out.println(numbers);
+        //new array list numbers2
+        ArrayList<Integer> numbers2 = new ArrayList<>();
+        //add elements to the array list
+        numbers2.add(10);
+        numbers2.add(20);
+        numbers2.add(30);
+        System.out.println(numbers2);
+        //new array list numbers3 collection from array list numbers2
+        ArrayList<Integer> numbers3 = new ArrayList<>(numbers2);
+        System.out.println(numbers3);
     }
 }
