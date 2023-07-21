9026f6b009308f51bc198a09adae7222dc4eedc9
BrockAltug <brock.altug99@gmail.com>
2023-07-21 02:07:06 -0700

    Assignment08 - method to get users name and checks if it is contained within a sentence

--- src/javaprojects/project05/Assignment08.java
+++ src/javaprojects/project05/Assignment08.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 //create a method to get users name and checks if it is contained within a sentence
 public class Assignment08 {
     public static void main(String[] args) {
+        //prompt user to send email (containing the name of the person)
         System.out.println("Send an email");
-        //checks if the users input contains the name
+        //checks if the users input contains the name using getMyName() method
         if(getMyName().contains("John Doe")){
+            //the name is contained in the users message
             System.out.println("Read This Email");
         }
         else {
+            //else the name is not contained in the message
             System.out.println("Do Not Read This Email");
         }
     }
-
     public static String getMyName(){
         //new scanner for user input
         Scanner input = new Scanner(System.in);
         //new string to store users input
         String userInput = input.nextLine();
-        //new String for name
-        String name = "John Doe";
+        //return users input for name
         return userInput;
     }
 }
