4be9768334fb04a2c374a04feb76301a35bc862b
BrockAltug <brock.altug99@gmail.com>
2023-09-20 02:22:21 -0700

    useful notes for java - Array List, asList, add, clear, set, get, size, contains, indexOf

--- src/JavaCodeNotes.java
+++ src/JavaCodeNotes.java
@@ -702,6 +702,14 @@ public class JavaCodeNotes {
 //        System.out.println(words.size());
 //    //gets the last word in the array list
 //        System.out.println(words.get(words.size()-1));
+//ArrayList<String> cities = new ArrayList<>(Arrays.asList("Tokyo", "London", "Toronto"));
+//        System.out.println(cities);
+//    //changes the name of index 0 in the array list
+//        cities.set(0,"Renton");
+//        System.out.println(cities);
+//    //changes and displays the name of index 1 in the array list - displays which one is being changed
+//        System.out.println(cities.set(1,"Seattle"));
+//        System.out.println(cities);
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
