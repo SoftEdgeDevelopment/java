ebf1eeb473556e5205e0c11ce5ca2fee7c29987d
BrockAltug <brock.altug99@gmail.com>
2023-09-25 02:27:29 -0700

    useful notes for java - array lists methods to display count of a specific search

--- src/JavaCodeNotes.java
+++ src/JavaCodeNotes.java
@@ -738,6 +738,49 @@ public class JavaCodeNotes {
 //    //removes multiple entries searching from the array list
 //        numbers.removeAll(Arrays.asList(5,10,15,22,25,66,98));
 //        System.out.println("numbers after removing multiple indexes " +numbers);
+
+    //--------------------------------------------- count example
+//    //method to display all cities starting with a specific letter and another method to display count
+//
+//    public class CountCities {
+//        public static void main(String[] args) {
+//            //new array list as list with city names
+//            ArrayList<String> cities = new ArrayList<>(Arrays.asList("Auburn", "Seattle", "Boston", "Aberdeen", "Tokyo"));
+//            //new array list aCities calling to the citiesStartingWithA method using the cities array list
+//            ArrayList<String> aCities = citiesStartingWithA(cities);
+//            //display aCities
+//            System.out.println("Cities starting with 'A': " + aCities);
+//            //call to the method to display the number of cities
+//            System.out.println(numOfACities(cities) + " cities");
+//        }
+//
+//        //array list method using city array list to get all cities starting with A
+//        public static ArrayList<String> citiesStartingWithA(ArrayList<String> cityList) {
+//            ArrayList<String> aCities = new ArrayList<>();
+//            //foreach city in the array list
+//            for (String city : cityList) {
+//                //if the city starts with A
+//                if (city.startsWith("A")) {
+//                    //add the city to the aCities array list
+//                    aCities.add(city);
+//                }
+//            }
+//            return aCities;
+//        }
+//
+//        //array list method to display number of cities starting with A
+//        public static int numOfACities(ArrayList<String> citiesList) {
+//            //starting counter for number of cities starting with A
+//            int counter = 0;
+//            //foreach city in the array list
+//            for (String city : citiesList) {
+//                if (city.substring(0,1).equalsIgnoreCase("a")){
+//                    counter++;
+//                }
+//            }
+//            return counter;
+//        }
+//    }
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
