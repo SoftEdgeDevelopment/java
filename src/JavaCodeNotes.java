3c554e2eaca0d1af241406fb818e2c7e589b4f98
BrockAltug <brock.altug99@gmail.com>
2023-10-05 02:59:08 -0700

    useful notes for java - linear search method to display array list information from another class

--- src/JavaCodeNotes.java
+++ src/JavaCodeNotes.java
@@ -1,3 +1,5 @@
+import unit07.arraylist.learning30.linearsearch.Student;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -815,6 +817,74 @@ public class JavaCodeNotes {
 //            numbers = oddNumbers;
 //
 //            System.out.println("Numbers after duplicating odds: " + numbers);
+//        }
+//    }
+//      -------------------Array List Linear Search Method------------------------------------
+
+//    //linear search method to display array list information from another class
+//
+//    public class LinearSearch {
+//        public static void main(String[] args) {
+//            //new student objects created from the Student class with name and id
+//            unit07.arraylist.learning30.linearsearch.Student student1 = new unit07.arraylist.learning30.linearsearch.Student("John Jones","QU23456");
+//            unit07.arraylist.learning30.linearsearch.Student student2 = new unit07.arraylist.learning30.linearsearch.Student("Kate Spade","IG82938");
+//            unit07.arraylist.learning30.linearsearch.Student student3 = new unit07.arraylist.learning30.linearsearch.Student("Mark Mike","GH92345");
+//            //new array list classroom calling to the student class, as list with the student objects
+//            ArrayList<unit07.arraylist.learning30.linearsearch.Student> classroom = new ArrayList<>(Arrays.asList(student1,student2,student3));
+//
+//            System.out.println(classroom);
+//            //calling to the findStudent method using classroom array list and students ID number, returns the students index
+//            System.out.println(findStudent(classroom,"GH92345"));
+//            //returns -1 because the student ID is not found in the classroom
+//            System.out.println(findStudent(classroom,"AA52347"));
+//        }
+//
+//        //new method to find the Students ID
+//        public static int findStudent(ArrayList<unit07.arraylist.learning30.linearsearch.Student> students, String id){
+//            for (int i = 0; i < students.size(); i++) {
+//                //in the students array list, get the index element, and get the ID as a string, and check if the ID matches
+//                if(students.get(i).getId().equals(id)){
+//                    //return the found id
+//                    return i;
+//                }
+//            }
+//            //the id is not found
+//            return -1;
+//        }
+//    }
+//
+//    //new class created for Students with private Strings for name and id as variables
+//    class Student {
+//        private String name;
+//        private String id;
+//
+//        //generate constructor
+//        public Student(String name, String id) {
+//            this.name = name;
+//            this.id = id;
+//        }
+//
+//        //create getters and setters
+//        public String getName() {
+//            return name;
+//        }
+//
+//        public void setName(String name) {
+//            this.name = name;
+//        }
+//
+//        public String getId() {
+//            return id;
+//        }
+//
+//        public void setId(String id) {
+//            this.id = id;
+//        }
+//
+//        //override to string to display information
+//        @Override
+//        public String toString() {
+//            return "Student{" + "name='" + name + '\'' + ", id='" + id + '\'' + '}';
 //        }
 //    }
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
