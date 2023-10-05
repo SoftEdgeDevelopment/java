39d976edddf89d974e49ace929919833d8eb2b95
BrockAltug <brock.altug99@gmail.com>
2023-10-05 04:25:29 -0700

    useful notes for java - String split and join methods

--- src/JavaCodeNotes.java
+++ src/JavaCodeNotes.java
@@ -881,8 +881,8 @@
 //            return "Student{" + "name='" + name + '\'' + ", id='" + id + '\'' + '}';
 //        }
 //    }
-// -------------------------------String Split Methods -------------------------------------------------------
-//public class SplitPractice {
+// -------------------------------String Split and Join Methods -------------------------------------------------------
+//public class JoinMethodExample {
 //    public static void main(String[] args) {
 //        String sentence = "Today I went to the store and bought an apple";
 //        //sentence split
@@ -893,10 +893,18 @@
 //            // +
 //            // the word of the substring starting from the first index
 //            System.out.println(word.substring(0,1).toUpperCase() + word.substring(1));
-//
 //        }
-//    }
+//        //new string for the updated sentence
+//        //delimiter - what goes between each element
+//        //elements - select the elements to join
+//        String updatedSentence = String.join(" ",words);
+//        System.out.println("After Joining: \n" + updatedSentence);
 //
+//        String[] someWords = {"Hello", "World", "of", "Java"};
+//        //uses a "-" to join each word in the array
+//        System.out.println(String.join("-", someWords));
+//    }
+//}
 //
 //}
 //public class SplitMethodExample {
