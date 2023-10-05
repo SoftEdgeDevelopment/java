6dc2b6f3b907506ef5480d0d96a773a3549f2535
BrockAltug <brock.altug99@gmail.com>
2023-10-05 04:30:33 -0700

    useful notes for java - method to parse to do math with Strings

--- src/JavaCodeNotes.java
+++ src/JavaCodeNotes.java
@@ -132,6 +132,23 @@
 //      --
 //      //4 is the starting index location, the random numbers will be between 5 total values starting from 4 (4,5,6,7,8) 8 is the highest possible number
 //      System.out.println((int) (Math.random() * 5) + 4);
+//
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+//       method to parse to do math with Strings
+
+//public class ParseIntMethod {
+//    public static void main(String[] args) {
+//        String numberString1 = "34";
+//        String numberString2 = "35";
+//        //will display the numbers as a string
+//        System.out.println(numberString1+numberString2);
+//        //parse for int to do math with Strings
+//        int num1 = Integer.parseInt(numberString1);
+//        int num2 = Integer.parseInt(numberString2);
+//        //will add the numbers and display the sum
+//        System.out.println(num1+num2);
+//    }
+//}
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 //        If, Else If, Else ---
 //        ---
