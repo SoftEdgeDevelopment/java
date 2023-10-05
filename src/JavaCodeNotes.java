9637e7c8cebe40b9bcc32e1a0920947dcfdb6023
BrockAltug <brock.altug99@gmail.com>
2023-10-05 03:45:49 -0700

    useful notes for java - String split methods

--- src/JavaCodeNotes.java
+++ src/JavaCodeNotes.java
@@ -881,5 +881,46 @@
 //            return "Student{" + "name='" + name + '\'' + ", id='" + id + '\'' + '}';
 //        }
 //    }
+// -------------------------------String Split Methods -------------------------------------------------------
+//public class SplitPractice {
+//    public static void main(String[] args) {
+//        String sentence = "Today I went to the store and bought an apple";
+//        //sentence split
+//        String[] words = sentence.split(" ");
+//        //for each word in the words array list
+//        for (String word : words) {
+//            //use substring to get the index of the first letter and change to upper case
+//            // +
+//            // the word of the substring starting from the first index
+//            System.out.println(word.substring(0,1).toUpperCase() + word.substring(1));
+//
+//        }
+//    }
+//
+//
+//}
+//public class SplitMethodExample {
+//    public static void main(String[] args) {
+//
+//        String sentence = "SDET stands for Software Development Engineer in Testing";
+//        //new string array for words calling to sentence.split will split the string based on the regress - (every space)
+//        String[] words = sentence.split(" ");
+//        //this will not work to display the array list
+//        System.out.println(words);
+//        //must do Arrays.toString
+//        System.out.println(Arrays.toString(words));
+//
+//        //another way of writing each word in a for loop
+//        for (String word : words) {
+//            System.out.println(word);
+//        }
+//
+//        //way to display the amount of words in a string
+//        String anotherSentence = "Testing and development goes hand by hand";
+//        String[] newWords = anotherSentence.split(" ");
+//        System.out.println("the length of the array is: " + newWords.length);
+//    }
+//
+//}
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 
