cdf8e94f8aec0f112d50323ac8dc8543db57afc8
BrockAltug <brock.altug99@gmail.com>
2023-10-06 01:31:41 -0700

    useful notes for java - different collections methods

--- src/JavaCodeNotes.java
+++ src/JavaCodeNotes.java
@@ -1247,5 +1247,22 @@
 //        System.out.println(Arrays.binarySearch(numbers,9898));
 //    }
 //}
+////different Collections methods
+//
+//public class SortReverseBinarySearch {
+//    public static void main(String[] args) {
+//        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(23, 35, 2, 35, 6, 34, 64, 57, 4, 57));
+//        System.out.println("original list: "+numbers);
+//        //import from Collections to sort the numbers
+//        Collections.sort(numbers);
+//        System.out.println("sorted list: "+numbers);
+//        //import from Collections to reverse the numbers
+//        Collections.reverse(numbers);
+//        System.out.println("reversed list"+numbers);
+//        Collections.reverse(numbers);
+//        System.out.println("reversed again for binary search: "+numbers);
+//        System.out.println("index of search key is: "+Collections.binarySearch(numbers,6));
+//    }
+//}
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 
