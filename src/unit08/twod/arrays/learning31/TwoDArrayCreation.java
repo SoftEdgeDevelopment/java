afc99c601bdd5da578e72d45ebd0a82dffc94c7a
BrockAltug <brock.altug99@gmail.com>
2023-10-06 01:39:52 -0700

    TwoDArrayCreation - how to make a 2d array

---
+++ src/unit08/twod/arrays/learning31/TwoDArrayCreation.java
@@ -0,0 +1,12 @@
+package unit08.twod.arrays.learning31;
+
+//how to make a 2d array
+
+public class TwoDArrayCreation {
+    public static void main(String[] args) {
+        //array for numbers
+        int[] numbers;
+        //2d array for arrayOfNumbers, contains 5 indexes holding 3 elements each
+        int[][] arrayOfNumbers = new int [5][3];
+    }
+}
