15223191a4210c5e3b3232cc3cc862ff3d841ce6
BrockAltug <brock.altug99@gmail.com>
2023-10-06 01:48:28 -0700

    PracticeCreationInitialization - practice with creation and initialization of 2d arrays

---
+++ src/unit08/twod/arrays/learning31/twodarraycreation/PracticeCreationInitialization.java
@@ -0,0 +1,14 @@
+package unit08.twod.arrays.learning31.twodarraycreation;
+
+//practice with creation and initialization of 2d arrays
+
+public class PracticeCreationInitialization {
+    public static void main(String[] args) {
+        //String array with 5 indexes of 12 elements
+        String[][] words = new String[5][12];
+        //int array with 10 indexes of 2 elements
+        int[][] numbers = new int[10][2];
+        //boolean array with 4 indexes of 4 elements
+        boolean[][] votes = new boolean[4][4];
+    }
+}
