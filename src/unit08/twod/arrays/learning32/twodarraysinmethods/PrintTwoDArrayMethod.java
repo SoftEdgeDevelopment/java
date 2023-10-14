e8b565d2fc52d4b642404b7e5a37bfcc6d0830dc
BrockAltug <brock.altug99@gmail.com>
2023-10-14 16:34:07 -0700

    PrintTwoDArrayMethod - method to produce and print the contents of a 2D array

--- src/unit08/twod/arrays/learning32/twodarraysinmethods/PrintTwoDArrayMethod.java
+++ src/unit08/twod/arrays/learning32/twodarraysinmethods/PrintTwoDArrayMethod.java
@@ -2,7 +2,7 @@ package unit08.twod.arrays.learning32.twodarraysinmethods;
 
 import java.util.Arrays;
 
-//method to print the contents of a 2d array
+//method to print the contents of a 2D array
 
 public class PrintTwoDArrayMethod {
     public static void main(String[] args) {
@@ -14,11 +14,22 @@ public class PrintTwoDArrayMethod {
         };
         //call to the method to print the 2d array
         printTwoDArray(words);
+        //call to the produceTwoDStringArray method to produce a new String array
+        System.out.println(Arrays.deepToString(produceTwoDStringArray()));
     }
 
     //new void method to print 2d array with 2d String array as parameter
     public static void printTwoDArray(String[][] arr){
         //using Arrays.deepToString() for 2d array
         System.out.println(Arrays.deepToString(arr));
+    }
+
+    //new 2d String array method to produce a new 2d String array
+    public static String[][] produceTwoDStringArray(){
+        //create new 2d String array
+        String[][] a2DArray = new String[5][2];
+        //return the 2d array back to the method
+        return a2DArray;
+        //return new String[5][2];
     }
 }
