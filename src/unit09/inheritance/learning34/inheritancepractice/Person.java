c204c098d2c9a1e2cc6d2fa9cde7367f8d9e18c4
BrockAltug <brock.altug99@gmail.com>
2023-10-16 01:01:12 -0700

    creating inheritance subclasses extending from Person superclass and a Main Class

---
+++ src/unit09/inheritance/learning34/inheritancepractice/Person.java
@@ -0,0 +1,12 @@
+package unit09.inheritance.learning34.inheritancepractice;
+
+//superclass
+public class Person {
+    public void study(){
+        //method used to get the class name (Person)
+        System.out.println(this.getClass().getSimpleName() + " is studying");
+    }
+}
+
+
+
