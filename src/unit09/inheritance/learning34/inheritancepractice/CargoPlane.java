591f3cb729f5d54cbcf02a9ebb0882dabdb1edbb
BrockAltug <brock.altug99@gmail.com>
2023-10-16 00:51:13 -0700

    creating inheritance subclasses extending from FlyingVehicle superclass

---
+++ src/unit09/inheritance/learning34/inheritancepractice/CargoPlane.java
@@ -0,0 +1,5 @@
+package unit09.inheritance.learning34.inheritancepractice;
+
+//inheritance subclass extends from Flying vehicle
+public class CargoPlane extends FlyingVehicle{
+}
---
+++ src/unit09/inheritance/learning34/inheritancepractice/FlyingVehicle.java
@@ -0,0 +1,11 @@
+package unit09.inheritance.learning34.inheritancepractice;
+
+import java.util.ArrayList;
+
+public class FlyingVehicle {
+    private ArrayList<String> type;
+
+    public FlyingVehicle(){
+        type = new ArrayList<>();
+    }
+}
