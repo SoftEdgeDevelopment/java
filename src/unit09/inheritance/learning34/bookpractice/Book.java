2fab3990d50b59e10384ffe97a23fb99520604f1
BrockAltug <brock.altug99@gmail.com>
2023-10-16 02:38:59 -0700

    creating subclass objects inheriting values from superclass

---
+++ src/unit09/inheritance/learning34/bookpractice/Book.java
@@ -0,0 +1,17 @@
+package unit09.inheritance.learning34.bookpractice;
+
+public class Book {
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
