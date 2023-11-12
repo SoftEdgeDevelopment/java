23d84d50479141a5e83abe327f122717930a5c0e
BrockAltug <brock.altug99@gmail.com>
2023-11-11 19:08:12 -0800

    Main - practice with abstract class creation

---
+++ src/unit10/abstractandinterface/learning39/abstractclasses/AnimalAbstract.java
@@ -0,0 +1,6 @@
+package unit10.abstractandinterface.learning39.abstractclasses;
+
+//new abstract class
+public abstract class AnimalAbstract {
+    public abstract void animalSound();
+}
---
+++ src/unit10/abstractandinterface/learning39/abstractclasses/Main.java
@@ -0,0 +1,8 @@
+package unit10.abstractandinterface.learning39.abstractclasses;
+
+public class Main {
+    public static void main(String[] args) {
+        //cannot create objects from abstract class
+        //AnimalAbstract animalAbstract = new AnimalAbstract();
+    }
+}
