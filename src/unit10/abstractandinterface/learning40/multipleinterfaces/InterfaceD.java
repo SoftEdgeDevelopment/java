2615ef26555d0cfbaf8edb933a04e6e76510e5a9
BrockAltug <brock.altug99@gmail.com>
2023-11-25 09:46:56 -0800

    A - multiple interfaces example

---
+++ src/unit10/abstractandinterface/learning40/multipleinterfaces/InterfaceD.java
@@ -0,0 +1,6 @@
+package unit10.abstractandinterface.learning40.multipleinterfaces;
+
+//interfaceD
+public interface InterfaceD {
+    void methodD();
+}
---
+++ src/unit10/abstractandinterface/learning40/multipleinterfaces/SuperClass.java
@@ -0,0 +1,11 @@
+package unit10.abstractandinterface.learning40.multipleinterfaces;
+
+//must implement method to make superclass abstract and override methods
+public abstract class SuperClass implements InterfaceB{
+
+    //@Override
+    //public void methodB() {
+
+    //}
+
+}
