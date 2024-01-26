8260a271c14c21a189f127505391496956d46400
Brock <brock.altug99@gmail.com>
2024-01-25 19:05:44 -0800

    CarPriorityMain - car priorityque example with car class

---
+++ src/unit11/collections/learning43/priorityqueue/Car.java
@@ -0,0 +1,35 @@
+package unit11.collections.learning43.priorityqueue;
+
+public class Car {
+    private String make;
+    private int year;
+
+    public Car(String make, int year) {
+        this.make = make;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
