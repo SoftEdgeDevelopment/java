5906b7ae0c7825d234e854129901ad25c88e06dc
Brock <brock.altug99@gmail.com>
2024-01-25 18:40:10 -0800

    PriorityQueueIntegers - string priority que example

---
+++ src/unit11/collections/learning43/priorityqueue/PriorityQueueString.java
@@ -0,0 +1,39 @@
+package unit11.collections.learning43.priorityqueue;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueueString {
+    public static void main(String[] args) {
+        //first index will ALWAYS be the smallest letter in alphabetical order
+        //new PriorityQueue using String Queue
+        Queue<String> priorityString = new PriorityQueue<>();
+        priorityString.offer("Tokyo");
+        priorityString.offer("Trabzon");
+        priorityString.offer("New Youk");
+        priorityString.offer("New Orleans");
+        priorityString.offer("Orlando");
+        priorityString.offer("Alabama");
+        priorityString.offer("Virginia");
+        System.out.println(priorityString);
+        System.out.println("-".repeat(15));
+        System.out.println("poll from PriorityQueue");
+        System.out.println(priorityString.poll());
+        System.out.println("-".repeat(15));
+        System.out.println("peek next in PriorityQueue");
+        System.out.println(priorityString.peek());
+        System.out.println("-".repeat(15));
+        System.out.println("poll from PriorityQueue");
+        System.out.println(priorityString.poll());
+        System.out.println("-".repeat(15));
+        System.out.println("poll from PriorityQueue");
+        System.out.println(priorityString.poll());
+        System.out.println("-".repeat(15));
+        System.out.println("peek next in PriorityQueue");
+        System.out.println(priorityString.peek());
+
+
+
+
+    }
+}
