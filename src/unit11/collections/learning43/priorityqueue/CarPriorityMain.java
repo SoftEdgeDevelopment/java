06d9ace91384ff100e869bd8a169795b40f566e0
Brock <brock.altug99@gmail.com>
2024-01-27 22:53:27 -0800

    CarPriorityMain - car priorityque example with car class

--- src/unit11/collections/learning43/priorityqueue/CarPriorityMain.java
+++ src/unit11/collections/learning43/priorityqueue/CarPriorityMain.java
@@ -8,5 +8,6 @@ public class CarPriorityMain {
     public static void main(String[] args) {
         //create a PriorityQueue by Comparator, comparing the Car class getYear method
         Queue<Car> carsByYear = new PriorityQueue<>(Comparator.comparing(Car::getYear));
+
     }
 }
