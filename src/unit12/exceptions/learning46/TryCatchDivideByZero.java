c9f2920aa0f15634174ca013786bd56cc24bc2a0
Brock <brock.altug99@gmail.com>
2024-02-10 04:25:26 -0800

    TryCatchDivideByZero - try catch example to catch divide by zero exception

--- src/unit12/exceptions/learning46/TryCatchDivideByZero.java
+++ src/unit12/exceptions/learning46/TryCatchDivideByZero.java
@@ -5,7 +5,17 @@ public class TryCatchDivideByZero {
         //try catch example to catch divide by zero exception
         try{
             System.out.println(10/0);
-        } catch (Exception exception){
+        }
+        //exception extending arithmeticException extending RunTimeException
+        //same thing as writing catch (Exception e)
+        catch (RuntimeException exception){
+
+            //printStackTrace() to display where the exception occured
+            exception.printStackTrace();
+
+            //use getMessage() to display the exception message
+            System.out.println("The exception message is " + exception.getMessage());
+
             System.out.println("Cannot divide by zero...");
         }
         System.out.println("After try, catch block...");
