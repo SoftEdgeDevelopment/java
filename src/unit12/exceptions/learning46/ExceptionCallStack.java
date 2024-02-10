8dfb836ff0298e9125976e14c12d71509be07b20
Brock <brock.altug99@gmail.com>
2024-02-10 02:31:00 -0800

    ExceptionCallStack - example how to read exceptions and the stack order which they appear

--- src/unit12/exceptions/learning46/ExceptionCallStack.java
+++ src/unit12/exceptions/learning46/ExceptionCallStack.java
@@ -2,19 +2,19 @@ package unit12.exceptions.learning46;
 
 public class ExceptionCallStack {
     public static void main(String[] args) {
-        //at unit12.exceptions.learning46.ExceptionCallStack.main
+        //at unit12.exceptions.learning46.ExceptionCallStack.main(ExceptionCallStack.java:6)
         dividerCaller();
 
         System.out.println("--After the Exception--");
     }
 
     //make an exception using a method
-    //at unit12.exceptions.learning46.ExceptionCallStack.divider
+    //at unit12.exceptions.learning46.ExceptionCallStack.divider(ExceptionCallStack.java:14)
     public static void divider(){
         System.out.println(10/0);
     }
 
-    //at unit12.exceptions.learning46.ExceptionCallStack.dividerCaller
+    //at unit12.exceptions.learning46.ExceptionCallStack.dividerCaller(ExceptionCallStack.java:19)
     public static void dividerCaller(){
         divider();
     }
