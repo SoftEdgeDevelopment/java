fc49c76ac8ed892c09443b943b9c3760b6a97de7
Brock <brock.altug99@gmail.com>
2024-02-10 07:59:10 -0800

    FinallyBlock - try catch example with finally throwing an ArithmeticException

--- src/unit12/exceptions/learning47/FinallyBlock.java
+++ src/unit12/exceptions/learning47/FinallyBlock.java
@@ -3,13 +3,18 @@ package unit12.exceptions.learning47;
 public class FinallyBlock {
     public static void main(String[] args) {
         //try catch example with finally throwing an ArithmeticException
-        try{
+        try {
             System.out.println("Before the exception.");
-            System.out.println(10/0);
-        }catch (ArithmeticException e){
+            System.out.println(10 / 0);
+            String word = null;
+            word.length();
+        } catch (ArithmeticException e) {
             System.out.println("An arithmetic exception has occurred...");
+            //will not be displayed if ArithmeticException is caught first
+        } catch (NullPointerException e) {
+            System.out.println("A null pointer exception has occurred...");
             //finally is always executed
-        }finally {
+        } finally {
             System.out.println("The code from the finally block");
         }
         System.out.println("End of try/catch/finally");
