e2d4fff57e7569b04efff875a59d31bbc4480074
Brock <brock.altug99@gmail.com>
2024-02-10 07:34:52 -0800

    MultipleCatchBlocks - multiple catch blocks example, ArrayIndexOutOfBoundsException, NullPointerException

---
+++ src/unit12/exceptions/learning47/MultipleCatchBlocks.java
@@ -0,0 +1,27 @@
+package unit12.exceptions.learning47;
+
+public class MultipleCatchBlocks {
+    public static void main(String[] args) {
+
+        //multiple catch blocks example, ArrayIndexOutOfBoundsException, NullPointerException
+        try{
+            //ArrayIndexOutOfBoundsException
+            int[] numbers = new int[3];
+            numbers[5] = 7;
+            //NullPointerException
+            String userString = null;
+            userString.length();
+        }catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+            System.out.println("Array index out of bound exception happened...");
+
+            //when NullPointerException occurs
+        }catch (NullPointerException e){
+            e.printStackTrace();
+            System.out.println("Null pointer exception occured");
+        }
+        System.out.println("*".repeat(15));
+        System.out.println("After try... catch block");
+        System.out.println("*".repeat(15));
+    }
+}
